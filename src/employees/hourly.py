8fa49b17d4c9e739022bf1a06edc881dd58ad221
andersonclementte <anderson.clementte@gmail.com>
2021-04-11 16:25:35 -0300

    change in attributes to private

--- src/employees/hourly.py
+++ src/employees/hourly.py
@@ -3,8 +3,8 @@ from employees.employee import Employee
 class Hourly(Employee):
     def __init__(self, name, address, kind, salary, extraHoursFactor):
         super().__init__(name, address, kind)
-        self.salary = salary
-        self.extraHoursFactor = extraHoursFactor
+        self._salary = salary
+        self._extraHoursFactor = extraHoursFactor
 
 # k1 = Hourly("Rafa", "Matao", "Trabaio", 500, 1.04)
 # print(k1)
--- src/employees/salaried.py
+++ src/employees/salaried.py
@@ -3,13 +3,15 @@ from employees.employee import Employee
 class Salaried(Employee):
     def __init__(self, name, address, kind, salary):
         super().__init__(name, address, kind)
-        self.salary = salary
+        self._salary = salary
+    
+    
 
 
 class Comissioned(Salaried):
     def __init__(self, name, address, kind, salary, bonus):
         super().__init__(name, address, kind, salary)
-        self.bonus = bonus
+        self._bonus = bonus
 
 # g1 = Comissioned("carlos", "Colina", "Comissionado", 4000, 500)
 # print(g1.bonus)
