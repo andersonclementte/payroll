21c9ee85721f2aaa90d865a31963e992bb5f3e40
andersonclementte <anderson.clementte@gmail.com>
2021-04-11 18:11:28 -0300

    update in properties and str method

--- src/employees/hourly.py
+++ src/employees/hourly.py
@@ -1,10 +1,14 @@
 from employees.employee import Employee
 
 class Hourly(Employee):
-    def __init__(self, name, address, kind, salary, extraHoursFactor):
-        super().__init__(name, address, kind)
+    def __init__(self, name, address, salary):
+        super().__init__(name, address)
+        self.kind = "Horista"
         self._salary = salary
-        self._extraHoursFactor = extraHoursFactor
+        self._extraHoursFactor = 1.5
 
-# k1 = Hourly("Rafa", "Matao", "Trabaio", 500, 1.04)
+    def __str__(self):
+        return super().__str__() + 'Tipo de empregado: {}'.format(self.kind)
+
+# k1 = Hourly("Rafa", "Matao", 500, 1.04)
 # print(k1)
--- src/employees/salaried.py
+++ src/employees/salaried.py
@@ -1,18 +1,25 @@
 from employees.employee import Employee
 
 class Salaried(Employee):
-    def __init__(self, name, address, kind, salary):
-        super().__init__(name, address, kind)
+    def __init__(self, name, address, salary):
+        super().__init__(name, address)
+        self.kind = "Assalariado"
         self._salary = salary
+    
+    def __str__(self):
+        return super().__str__() + 'Tipo de empregado: {}'.format(self.kind)
     
     
 
 
 class Comissioned(Salaried):
-    def __init__(self, name, address, kind, salary, bonus):
-        super().__init__(name, address, kind, salary)
+    def __init__(self, name, address, salary, bonus):
+        super().__init__(name, address, salary)
+        self.kind = "Comissionado"
         self._bonus = bonus
 
-# g1 = Comissioned("carlos", "Colina", "Comissionado", 4000, 500)
-# print(g1.bonus)
-# print(g1.name)
+    def __str__(self):
+        return super().__str__()
+
+# g1 = Comissioned("carlos", "Colina", 4000, 500)
+# print(g1)
