692950bdbe687d379f1057047ee08ec743c812a5
andersonclementte <anderson.clementte@gmail.com>
2021-04-11 18:10:50 -0300

    update in properties

--- src/employees/employee.py
+++ src/employees/employee.py
@@ -1,14 +1,12 @@
 from abc import ABC, abstractmethod
 class Employee(ABC):
     @abstractmethod
-    def __init__(self, name, address, kind):
+    def __init__(self, name, address):
         self.name = name
-        self.id = id(self)
         self.address = address
-        self.kind = kind
 
     def companyEmail(self):
         return '{}@company.com'.format(self.name)
     
     def __str__(self):
-        return 'Nome: {}\nEndereco: {}\nTipo de funcionario: {}\nId: {}'.format(self.name, self.address, self.kind, self.id)
+        return 'Dados do funcionário:\nNome: {}\nEndereco: {}\n'.format(self.name, self.address)
