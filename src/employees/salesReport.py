e5bc2ab56e5f3cbe5f9bf3dfb22327b1fa40d2db
andersonclementte <anderson.clementte@gmail.com>
2021-04-13 21:32:12 -0300

    implemented output

--- src/employees/salesReport.py
+++ src/employees/salesReport.py
@@ -2,3 +2,6 @@ class SalesReport():
     def __init__(self, date, value):
         self.date = date
         self.value = value
+
+    def __str__(self):
+        return '(Data: {}, Valor: {})'.format(self.date, self.value)
